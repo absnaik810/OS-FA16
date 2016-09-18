53718d47d36777bcfba95fcb3e26d41098d77eeb
Abhishek Naik <ahnaik@indiana.edu>
2016-09-18 12:39:55 -0400

    Assignment1

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -4,15 +4,24 @@
 #include <string.h>
 #include <stdio.h>
 
+/*---------------------------------------------------------------------------------
+ *	xsh_hello - prints hello <name>; also provided with a --help argument
+ ---------------------------------------------------------------------------------*/
+
 shellcmd xsh_hello(int nargs, char *args[]) {
 
 	if(nargs==2)
-		printf("Hello %s\n", args[1]);
+	{
+		if(strcmp(args[1],"--help")==0)
+			printf("This is just a basic hello command.  Just type in 'hello' followed by your name; and the program will output Hello <name>\n");
+		else
+			printf("Hello %s\n", args[1]);
+	}
 	else
 		if(nargs<2)
-			printf("Arguments less than 2 entered\n");
+			printf("Less arguments entered\n");
 		else
-			printf("Arguments greater than 2 entered\n");
+			printf("Many arguments entered\n");
 
 	return 0;
 }
