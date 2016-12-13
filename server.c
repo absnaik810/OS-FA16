5144d02469f95292db5bc8558b51c3fe7d9684c1
Abhishek Naik <ahnaik@indiana.edu>
2016-12-12 22:52:39 -0500

    New changes

---
+++ server.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+
+int main()
+{
+
+	int socketfd, nbytes, from_len;
+	int portno = 3000;
+	struct sockaddr_in server_addr, from_addr;
+	char buff[512];
+	char rpy[512] ;
+	int len;
+
+	socketfd = socket(AF_INET,SOCK_DGRAM,0 );
+	if (socketfd < 0)
+		printf("Error in obtaining socket_fd");
+
+	server_addr.sin_family = AF_INET;
+     	server_addr.sin_addr.s_addr = INADDR_ANY;
+     	server_addr.sin_port = htons(portno);
+
+	if(bind(socketfd, (struct sockaddr*)&server_addr,sizeof(server_addr)) < 0)
+		printf(" Error in binding the socket");
+	
+	from_len = sizeof(from_addr);
+	printf("Server called: \n");
+	while(1)
+	{
+		bzero(buff,512);
+		printf("Waiting for the requests.\n");		
+		nbytes = recvfrom(socketfd, buff, sizeof(buff), 0, (struct sockaddr*)&from_addr, &from_len);
+		
+		if (nbytes < 0 )
+		{
+			printf("Error in recvfrom()");
+		}			
+		printf("String received at server: %s\n",buff);
+		len = strlen(buff);
+		snprintf(rpy,sizeof(rpy),"%d",len);
+
+		sendto(socketfd, rpy, strlen(rpy), 0,(struct sockaddr*)&from_addr,from_len);
+		memset(buff,'\0',sizeof(buff));
+
+		printf("Length of the received string sent!\n");
+	}
+
+	return 0;
+}
